cf98f8c21242e734628f61f9efa7366a4864ede7
neethu <neethump2014@gmail.com>
2016-03-09 16:28:02 +0530

    activity BestTimeService is created

---
+++ app/src/main/java/com/example/neethu/gcmnetworkmanager/BestTimeService.java
@@ -0,0 +1,8 @@
+package com.example.neethu.gcmnetworkmanager;
+
+/**
+ * Created by neethu on 9/3/16.
+ */
+public class BestTimeService {
+    private  static  final String TAG="bestTimeService";
+}
