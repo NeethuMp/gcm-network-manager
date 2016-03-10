70f923903cb2dce8e91967a34a79c8282b2f2772
neethu <neethump2014@gmail.com>
2016-03-10 11:17:49 +0530

    TaskItem is added

--- app/src/main/java/com/example/neethu/gcmnetworkmanager/TaskItem.java
+++ app/src/main/java/com/example/neethu/gcmnetworkmanager/TaskItem.java
@@ -4,44 +4,39 @@ package com.example.neethu.gcmnetworkmanager;
  * Created by neethu on 9/3/16.
  */
 public class TaskItem {
-    private  static  final String ONEOFF_TASK="oneoffTask";
-    private static final String NOW_TASK="nowTask";
-    private static final String EXECUTED_STATUS="executed";
-    private static final String FAILED_STATUS="failed";
-    private static final String PENDIND_STATUS="pending";
-    private String mID;
+    public   static  final String ONEOFF_TASK="oneoffTask";
+    public static final String NOW_TASK="nowTask";
+    public static final String EXECUTED_STATUS="executed";
+    public static final String FAILED_STATUS="failed";
+    public static final String PENDIND_STATUS="pending";
+    private String mId;
     private String mType;
     private String mStatus;
 
 
     public TaskItem(String id, String label, String status) {
-        mID = id;
+        mId = id;
         mType = label;
         mStatus = status;
     }
 
-    public String getmType() {
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        this.mId = id;
+    }
+
+    public String getType() {
         return mType;
     }
 
-    public void setmType(String mType) {
-        this.mType = mType;
-    }
-
-    public String getmID() {
-        return mID;
-    }
-
-    public void setmID(String mID) {
-        this.mID = mID;
-    }
-
-    public String getmStatus() {
+    public String getStatus() {
         return mStatus;
     }
 
-    public void setmStatus(String mStatus) {
-        this.mStatus = mStatus;
+    public void setStatus(String stat) {
+        this.mStatus = stat;
     }
-
 }
